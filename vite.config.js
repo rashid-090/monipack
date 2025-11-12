d0f68d43ee0a501d78064fd9c7db0b512aa4a51a
rashid-090 <rashidpk090@gmail.com>
2025-11-12 19:07:17 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+    esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  }
 })
